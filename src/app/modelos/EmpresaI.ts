57d54059ea3ed5af26b3b134184f360ac749d382
Billy Cuscul <bcuscul@centrans.gt>
2021-10-23 11:25:40 -0600

    Creacion de Modelo

---
+++ src/app/modelos/EmpresaI.ts
@@ -0,0 +1,10 @@
+export interface EmpresaI{
+    id:number;
+    nombre_comercial: string;
+    razon_social: string;
+    telefono: string;
+    correo: string;
+    nit: string;
+    direccion : string; 
+    estado: string;
+}
